5adab5021187089126cce0ed5e7e893c18db4901
ninawrona <90191093+ninawrona@users.noreply.github.com>
2022-04-20 14:29:53 +0200

    Methods in controllers

--- Main/src/View/GuestInformationController.java
+++ Main/src/View/GuestInformationController.java
@@ -1,4 +1,14 @@
 package View;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class GuestInformationController {
+    public TextField firstNameField;
+    public TextField lastNameField;
+    public TextField emailField;
+    public TextField phoneNumberField;
+
+    public void confirmButton(ActionEvent actionEvent) {
+    }
 }
--- Main/src/View/ReservationController.java
+++ Main/src/View/ReservationController.java
@@ -1,4 +1,17 @@
 package View;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
 public class ReservationController {
+    public DatePicker startDate;
+    public DatePicker endDate;
+    public Label errorLabel;
+
+    public void lookForAvailableRooms(ActionEvent actionEvent) {
+    }
+
+    public void reservationButton(ActionEvent actionEvent) {
+    }
 }
