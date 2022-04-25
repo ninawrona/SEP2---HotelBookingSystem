001bef62fee57675cba76d3542054b7094da6f55
namekarolis <sadeckas.karolisx@gmail.com>
2022-04-25 18:02:49 +0200

    ViewModel
    View:
    - added bindings
    - listView fix
    
    ViewModel:
    - datePicker fix
    - functionality to use model interface

--- Main/src/View/GuestInformationController.java
+++ Main/src/View/GuestInformationController.java
@@ -1,14 +1,45 @@
 package View;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+import viewModel.GuestInformationViewModel;
+import viewModel.ReservationViewModel;
+
+import java.rmi.RemoteException;
 
 public class GuestInformationController {
     public TextField firstNameField;
     public TextField lastNameField;
     public TextField emailField;
     public TextField phoneNumberField;
+    public Label errorLabel;
+    private GuestInformationViewModel viewModel;
+
+    public void init(ViewHandler viewHandler, ViewModelFactory viewModelFactory, Region root)
+            throws RemoteException {
+        this.viewHandler = viewHandler;
+        this.viewModelFactory = viewModelFactory;
+        this.viewModel = viewModelFactory.getGuestInformationViewModel();
+        this.root = root;
+        init();
+
+        // Binding
+        firstNameField.textProperty().bind(viewModel.getFirstNameField());
+        lastNameField.textProperty().bind(viewModel.getLastNameField());
+        emailField.textProperty().bind(viewModel.getEmailField());
+        phoneNumberField.textProperty().bind(viewModel.getPhoneNumberField());
+        errorLabel.textProperty().bind(viewModel.getErrorLabel());
+    }
 
     public void confirmButton(ActionEvent actionEvent) {
+        String possibleErrorMessage = viewModel.createGuest();
+        if (possibleErrorMessage.equals("Success")) {
+            // Go to next window
+            // viewHandler.openView(".fxml");
+        } else {
+            errorLabel.setText(possibleErrorMessage);
+        }
     }
 }
